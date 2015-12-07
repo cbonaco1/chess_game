3b64e81849d91a9f16629b887eacd3af20d4bf9c
App Academy Student <appacademy@Apps-Mac-mini-69.local>
2015-12-07 15:00:56 -0500

    Move position, implement 2-D array shorthand access, and check is_valid_move?.

--- chess/board.rb
+++ chess/board.rb
@@ -5,25 +5,51 @@ class Board
 
   def initialize
     @grid = Array.new(8) { Array.new(8) }
+    populate_board
+
+  end
+
+  # def [](*position)
+  #   grid[position[0]][position[1]]
+  # end
+
+  def populate_board
     [0,1,6,7].each do |row_index|
       grid[row_index].each_index do |col_index|
         grid[row_index][col_index] = Piece.new([row_index, col_index])
       end
     end
+  end
+
+  def [](pos)
+    row, col = pos
+    grid[row][col]
+  end
+
+  def []=(pos, mark)
+    row, col = pos
+    grid[row][col] = mark
+  end
+  #start => [2,4]
+  #end_pos => [3,5]
+  def move(start, end_pos)
+    #update the grid
+    #assume move is valid for now
+    if is_valid_move?(start, end_pos)
+      piece = self[start]
+      self[end_pos] = piece
+      self[start] = nil
+
+      #update the position of Piece
+      piece.position = end_pos
+    end
 
   end
 
-
-
-  def move(start, end_pos)
-    #update the grid
-
-
-    #update the position of Piece
-    #start => [2,4]
-    #end_pos => [3,5]
-
-    grid[end_pos] = grid[start]
+  def is_valid_move?(start, end_pos)
+    raise StandardError if self[start].nil?
+    #Implement logic to determine if move is valid for the Piece
+    true
   end
 
 
