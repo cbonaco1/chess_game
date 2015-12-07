52b48415fca8e16bef5ab4a841675cfe0544baaf
App Academy Student <appacademy@Apps-Mac-mini-69.local>
2015-12-07 16:36:54 -0500

    Implement move method to relocate object of Piece class.

--- chess/game.rb
+++ chess/game.rb
@@ -2,6 +2,9 @@ require_relative "board.rb"
 require_relative "player.rb"
 
 class Game
+
+  attr_accessor :board, :player
+
   def initialize
     @board = Board.new
     @player = Player.new(@board)
@@ -10,20 +13,19 @@ class Game
   def run
     puts "Mark all the spaces on the board!"
     puts "WASD or arrow keys to move the cursor, enter or space to confirm."
-    until @board.full?
+    until board.full?
       puts("Please select a piece.")
-        start_pos = @player.select_pos
+      start_pos = player.select_pos
       puts("Where would you like to move?")
-        end_pos = @player.select_pos
-      @board.move(start_pos, end_pos)
+      end_pos = player.select_pos
 
-      #gives position of current selection
+      board.move(start_pos, end_pos)
 
-      puts(pos)
-
+      
 
 
-      @board.mark(pos)
+
+      # @board.mark(pos)
     end
     puts "Hooray, the board is filled!"
   end
