3e260ecbfcba8a77be7204be869b459ddfe8bf11
App Academy Student <appacademy@Apps-Mac-mini-69.local>
2015-12-07 17:48:40 -0500

    Generate possible movements in moves method of SlidingPiece class.

--- chess/piece.rb
+++ chess/piece.rb
@@ -7,7 +7,7 @@ class Piece
 
   #Returns an array of possible locations it can go to
   def moves
-
+    #should return an array of places a Piece can move to
   end
 
   def present?
@@ -27,21 +27,65 @@ end
 
 class SlidingPiece < Piece
 
+  #should return an array of places a Piece can move to
+  def moves
+    #[1, 2]
+    current_row, current_column = self.position
+    up_down_moves = []
+    diagonal_moves = []
+
+    (0..7).each do |value|
+      #Up/Down
+      up_down_moves << [value, current_column]
+      #Left/Right
+      up_down_moves << [current_row, value]
+    end
+
+    #diagonal
+    (0..7).each do |value|
+      diagonal_moves << [current_row + value, current_column + value]
+      diagonal_moves << [current_row - value, current_column - value]
+      diagonal_moves << [current_row + value, current_column - value]
+      diagonal_moves << [current_row - value, current_column + value]
+    end
+
+    #filters for diagonals that are on board
+    diagonal_moves.select! do |array|
+      array[0].between?(0,7) && array[1].between?(0,7)
+    end
+    (up_down_moves + diagonal_moves).uniq
+  end
 end
 
 class Bishop < SlidingPiece
+
+
   def move_dirs
+    #filters for ONLY diagonal slides
   end
+
+  def moves
+    # super.select {|move| if move_dirs allows moves}
+  end
+
+
 end
 
 class Rook < SlidingPiece
+
   def move_dirs
+    #filters for horitizon and vertical slides
   end
+
+
 end
 
 class Queen < SlidingPiece
+
+  #slides in ALL directions
   def move_dirs
   end
+
 end
 
 
@@ -53,9 +97,13 @@ class SteppingPiece < Piece
 end
 
 class Knight < SteppingPiece
+  #moves in "L" shape
+
 end
 
 class King < SteppingPiece
+
+  #moves one step in any direction
 end
 
 
