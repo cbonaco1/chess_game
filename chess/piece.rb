fff14f0c02deea475a592c7ecf6a729aaf5a4ff9
App Academy Student <appacademy@Apps-Mac-mini-69.local>
2015-12-07 16:54:08 -0500

    Add skeleton for Piece class and Piece subclasses.

--- chess/piece.rb
+++ chess/piece.rb
@@ -5,6 +5,11 @@ class Piece
     @position = position
   end
 
+  #Returns an array of possible locations it can go to
+  def moves
+
+  end
+
   def present?
     true
   end
@@ -14,6 +19,50 @@ class Piece
   end
 
 end
+
+
+############################
+#      SLIDING PIECES      #
+############################
+
+class SlidingPiece < Piece
+
+end
+
+class Bishop < SlidingPiece
+  def move_dirs
+  end
+end
+
+class Rook < SlidingPiece
+  def move_dirs
+  end
+end
+
+class Queen < SlidingPiece
+  def move_dirs
+  end
+end
+
+
+#############################
+#      STEPPING PIECES      #
+#############################
+
+class SteppingPiece < Piece
+end
+
+class Knight < SteppingPiece
+end
+
+class King < SteppingPiece
+end
+
+
+
+
+
+
 
 
 class NullPiece
