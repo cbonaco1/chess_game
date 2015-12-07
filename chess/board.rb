b8588fc62d8d4f65e56ae10152a60df48b043407
App Academy Student <appacademy@Apps-Mac-mini-69.local>
2015-12-07 14:26:09 -0500

    Initialize both players side of grid with pieces.

--- chess/board.rb
+++ chess/board.rb
@@ -5,16 +5,25 @@ class Board
 
   def initialize
     @grid = Array.new(8) { Array.new(8) }
-    grid.each_with_index do |row, row_index|
-      row.each_index do |col_index|
+    [0,1,6,7].each do |row_index|
+      grid[row_index].each_index do |col_index|
         grid[row_index][col_index] = Piece.new([row_index, col_index])
       end
     end
+
   end
+
+
 
   def move(start, end_pos)
     #update the grid
+
+
     #update the position of Piece
+    #start => [2,4]
+    #end_pos => [3,5]
+
+    grid[end_pos] = grid[start]
   end
 
 
