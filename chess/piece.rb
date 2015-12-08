5e8e72aa44149b557a136509b1b3afbeb02c5795
App Academy Student <appacademy@Apps-Mac-mini-69.local>
2015-12-08 16:17:05 -0500

    Implement King and Knight moves method with proper restrictions.

--- chess/piece.rb
+++ chess/piece.rb
@@ -33,7 +33,22 @@ end
 
 class SlidingPiece < Piece
 
-  def down_moves
+  #should return an array of places a Piece can move to
+  def moves
+    up_down_moves = get_down_moves + get_up_moves
+    left_right_moves = get_left_moves + get_right_moves
+
+    #collective lists of moves
+    vert_hori_moves = up_down_moves + left_right_moves
+    diagonal_moves = get_diagonal_moves
+
+    total_possible_moves = (vert_hori_moves + diagonal_moves).uniq
+    total_possible_moves.select { |move| move_dirs(move) }
+
+  end
+
+  private
+  def get_down_moves
     current_row, current_column = self.position
     moves = []
     ((current_row + 1)..7).each do |row_num|
@@ -46,7 +61,7 @@ class SlidingPiece < Piece
     moves
   end
 
-  def up_moves
+  def get_up_moves
     current_row, current_column = self.position
     moves = []
     (current_row - 1).downto(0) do |row_num|
@@ -60,7 +75,7 @@ class SlidingPiece < Piece
     moves
   end
 
-  def left_moves
+  def get_left_moves
     current_row, current_column = self.position
     moves = []
     (current_column - 1).downto(0) do |col_num|
@@ -74,7 +89,7 @@ class SlidingPiece < Piece
     moves
   end
 
-  def right_moves
+  def get_right_moves
     current_row, current_column = self.position
     moves = []
     ((current_column + 1)..7).each do |col_num|
@@ -88,7 +103,7 @@ class SlidingPiece < Piece
     moves
   end
 
-  def diagonal_moves
+  def get_diagonal_moves
     current_row, current_column = self.position
     diag_moves = [ [-1, 1], [-1, -1], [1, -1], [1, 1] ]
     possible_moves = []
@@ -108,51 +123,13 @@ class SlidingPiece < Piece
     possible_moves
   end
 
-
-  #should return an array of places a Piece can move to
-  def moves
-    #[1, 2]
-    current_row, current_column = self.position
-    up_down_moves = down_moves + up_moves
-    diagonal_moves = []
-
-    # (0..7).each do |value|
-    #   #Up/Down
-    #   up_down_moves << [value, current_column]
-    #   #Left/Right
-    #   up_down_moves << [current_row, value]
-    # end
-
-    #diagonal
-    (0..7).each do |value|
-      diagonal_moves << [current_row + value, current_column + value]
-      diagonal_moves << [current_row - value, current_column - value]
-      diagonal_moves << [current_row + value, current_column - value]
-      diagonal_moves << [current_row - value, current_column + value]
-    end
-
-
-    #filters for diagonals that are on board
-    diagonal_moves.select! do |array|
-      array[0].between?(0,7) && array[1].between?(0,7)
-    end
-    total_possible_moves = (up_down_moves + diagonal_moves).uniq
-
-    total_possible_moves.select {|move| move_dirs(move) && !in_the_way?(move) }
-  end
-
-
-
-
 end
 
 class Bishop < SlidingPiece
 
   #filters for ONLY diagonal slides
   def move_dirs(move)
-
     current_row, current_col = self.position
-
     #NOT a diagnol move if the row or column is the same as current row/column
     return false if move.first == current_row || move.last == current_col
     true
@@ -193,17 +170,88 @@ end
 #############################
 
 class SteppingPiece < Piece
+  def moves
 
+  end
 end
 
 class Knight < SteppingPiece
   #moves in "L" shape
+  def moves
+    paths = []
+    knight_moves = [
+                    [2, -1],
+                    [2, 1],
+                    [-2, 1],
+                    [-2, -1],
+                    [1, 2],
+                    [1, -2],
+                    [-1, 2],
+                    [-1, -2]
+                  ]
+    current_row, current_column = self.position
+
+    knight_moves.each do |move|
+      path = []
+      row_ops = move.first
+      col_ops = move.last
+      row_factor = (row_ops <=> 0)
+      col_factor = (col_ops <=> 0)
+      row_num = current_row
+      col_num = current_column
+
+      (row_ops.abs).times do
+        row_num += row_factor
+        next if row_num < 0
+        path << [row_num, col_num] if row_num.between?(0, 7) && col_num.between?(0, 7)
+      end
+
+      (col_ops.abs).times do
+        col_num += col_factor
+        next if col_num < 0
+        path << [row_num, col_num] if col_num.between?(0, 7) && row_num.between?(0, 7)
+      end
+
+      # paths << path
+
+      paths << path.last if path.length == 3 && path.all? do |position|
+        board.grid[position[0]][position[1]].is_a?(NullPiece)
+      end
+
+    end
+    paths
+  end
 
 end
 
 class King < SteppingPiece
-
   #moves one step in any direction
+  def moves
+    current_row, current_column = self.position
+    possible_moves = []
+
+    king_moves = [
+                  [1,1],
+                  [1,-1],
+                  [1, 0],
+                  [0, 1],
+                  [0, -1],
+                  [-1, 0],
+                  [-1, -1],
+                  [-1, 1]
+                ]
+
+    king_moves.each do |move|
+      next_row = current_row + move.first
+      next_col = current_column + move.last
+      if next_row.between?(0, 7) && next_col.between?(0, 7)
+        if board.grid[next_row][next_col].is_a?(NullPiece)
+          possible_moves << [next_row, next_col]
+        end
+      end
+    end
+    possible_moves
+  end
 end
 
 
